387ccbbe199bae8e64e424f1d911e27f77d2a2a2
Renzo <renzopanduro00@gmail.com>
2024-11-26 10:23:58 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ import { app, auth, firestore } from '@/firebase'
 // Crear la instancia de la app Vue
 const vueApp = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+App.use(createPinia())
+App.use(router)
 
 // Usar Pinia y Router
 vueApp.use(createPinia())
